cd1b0c4a10fa45d1d7088d14399335b777c642ec
Pancham <75246178+Pancham555@users.noreply.github.com>
2025-10-08 16:29:54 +0530

    Made some initial changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
+import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
+import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
-  integrations: [tailwind(), compress()],
+  integrations: [mdx(), tailwind(), compress(), keystatic(), react(), 
+    icon({   include: ["iconamoon","mdi"],})
+  ],
 });
---
+++ keystatic.config.ts
@@ -0,0 +1,53 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title', // ✅ points to top-level field
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        // Title (used for slug)
+        title: fields.slug({
+          name: { label: 'Title' },
+        }),
+
+        // Short description / excerpt
+        description: fields.text({
+          label: 'Description',
+          multiline: true,
+          validation:{isRequired:true}
+        }),
+
+        // Publish date
+        date: fields.date({
+          label: 'Publish Date',
+          validation:{isRequired:true}
+        }),
+
+        // Featured image
+   image: fields.image({
+  label: 'Featured Image',
+  directory: 'public/assets/images/posts',
+  publicPath: '/assets/images/posts/',
+  validation:{isRequired:true}
+}),
+
+        // Full-width writing area (MDX content)
+        content: fields.mdx({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/posts',
+              publicPath: '../../assets/images/posts/',
+            },
+          },
+        }),
+      },
+    }),
+  },
+});
